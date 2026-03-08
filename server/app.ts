94a86c900dc62277634f0f76ef91b0df1df0e9e9
yoran dekoker <yoradeko@student.arteveldehs.be>
2026-03-08 22:16:32 +0100

    trying to fix the .json fetch and fixing mapstructure because routing was very wrong

--- server/app.ts
+++ server/app.ts
@@ -2,7 +2,6 @@ import path from "path";
 import express, { Application, Request, Response } from "express";
 import routes from "./routes";
 
-
 const app: Application = express();
 const PORT: number = 3000;
 
@@ -11,7 +10,9 @@ app.use(express.static(path.join(__dirname, "/public")));
 // Middleware om formulierdata te verwerken
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use("/", routes);
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
   console.log(`Server draait op http://localhost:${PORT}`);
--- server/routes.ts
+++ server/routes.ts
@@ -1,17 +1,69 @@
 import express, { Request, Response } from "express";
 import path from "path";
+import { getNews, getNewsBySlug } from "./newsService";
 
 const router = express.Router();
 
 /**
  * GET / - Laadt de homepagina
  */
-router.get("/", (req: Request, res: Response): void => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
+router.get("/", (req: Request, res: Response) => {
+
+  const news = getNews();
+
+  let html = `
+  <head>
+  <title>Nieuws</title>
+  <link rel="stylesheet" href="../css/main.css">
+</head>
+<body>
+
+  <h1>Nieuws</h1>
+  <ul>
+  </body>
+  `;
+
+  news.forEach(article => {
+    html += `<li>
+      <a href="/news/${article.slug}">
+        ${article.title}
+      </a>
+    </li>`;
+  });
+
+  html += `</ul>`;
+
+  res.send(html);
 });
 
-router.get("/details", (req: Request, res: Response): void => {
-  res.sendFile(path.join(__dirname, "/views/details.html"));
-});
+router.get("/news/:slug", (req: Request, res: Response) => {
 
+  const article = getNewsBySlug("" + req.params.slug);
+
+  if(!article){
+    res.status(404).send("Artikel niet gevonden");
+    return;
+  }
+
+  const html = `
+  <head>
+  <title>Nieuws</title>
+  <link rel="stylesheet" href="../css/main.css">
+</head>
+<body>
+
+  <h1>${article.title}</h1>
+  <p>${article.date}</p>
+  <p>${article.content}</p>
+
+  <a href="/">← terug</a>
+  </body>
+  `;
+
+  res.send(html);
+
+});
+// ik snapte echt niet hoe dit moest dus heb hierbij de hulp van AI ingeschakeld
 export default router;
+
+
